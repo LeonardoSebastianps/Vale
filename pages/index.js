edd124344fbb8955cb2129453b0e3a44ccbddeae
LeonardoSebastianps <83441780+LeonardoSebastianps@users.noreply.github.com>
2024-02-13 14:55:52 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,29 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Head>
         <title>TE AMO VAL</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className={styles.main}>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <div className={styles.imageContainer}>
+          <img src="/path/to/your/image.jpg" alt="Your Image" className={styles.image} />
+          <h2 className={styles.text}>Te amo</h2>
+        </div>
+        <p className={styles.description}>
+          Si es que quiere casarse conmigo
         </p>
+        <div className={styles.buttonContainer}>
+          <button className={styles.button}>Si</button>
+          <button className={styles.button}>No</button>
+        </div>
       </main>
 
       <Footer />
