3e8ac21b9384a4a9c25a38c3fc852d3a9b1d5243
LeonardoSebastianps <83441780+LeonardoSebastianps@users.noreply.github.com>
2024-02-13 15:08:25 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Header title="Te Amo Valeria Lizbeth Granados Robles" />
+        <Header title="Te amo Valeria Lizbeth Granados Robles" />
         <div className={styles.imageContainer}>
           <img src="https://i.pinimg.com/originals/21/a8/51/21a851b0616cfd12f1ec564c742af990.jpg" alt="Nosotros Miamor" className={styles.image} />
           <h2 className={styles.text}>Te amo</h2>
