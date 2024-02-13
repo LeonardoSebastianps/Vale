f6e1a0605fc9d0af59e98607a400daa132f53adf
LeonardoSebastianps <83441780+LeonardoSebastianps@users.noreply.github.com>
2024-02-13 15:42:01 -0600

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,31 +1,9 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
-import Header from '@components/Header';
-import Footer from '@components/Footer';
-
-const images = [
-  'https://i.pinimg.com/originals/21/a8/51/21a851b0616cfd12f1ec564c742af990.jpg',
-  'https://th.bing.com/th/id/OIP.2qLwfobRTgrDZWbvmndZxgHaEb?rs=1&pid=ImgDetMain',
-];
-
-const sizes = ['100%', '95%', '90%', '85%', '80%', '75%', '70%', '65%', '60%'];
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+import Image from 'next/image'
 
 export default function Home() {
-  const [answer, setAnswer] = useState('');
-  const [imageIndex, setImageIndex] = useState(0);
-  const [noButtonSize, setNoButtonSize] = useState('100%');
-
-  const handleAnswer = (newAnswer) => {
-    setAnswer(newAnswer);
-    setImageIndex((imageIndex + 1) % images.length);
-    setNoButtonSize(sizes[sizes.length - 1]);
-  };
-
-  const handleNoButtonClick = () => {
-    setNoButtonSize(sizes[sizes.indexOf(noButtonSize) - 1]);
-    handleAnswer('no');
-  };
-
   return (
     <div className="container">
       <Head>
@@ -35,34 +13,25 @@ export default function Home() {
 
       <main>
         <Header title="TE AMO!" />
-        {!answer ? (
-          <>
-            
-            <div className="centered-image">
-              <img src={images[imageIndex]} alt="Centered Image" />
-            </div>
-            <p className="description">
-               ¿Quieres casarte conmigo?
-            </p>
-          </>
-        ) : (
-          <p>You answered: {answer}</p>
-        )}
+        <div className="centered-image">
+          <Image
+            src="https://i.pinimg.com/originals/21/a8/51/21a851b0616cfd12f1ec564c742af990.jpg" // Replace with the path to your desired image
+            alt="Centered Image"
+            width={400}
+            height={300}
+          />
+        </div>
+        <div className="text-container">
+          <p>Te amo</p>
+          <p>¿Quieres casarte conmigo?</p>
+        </div>
         <div className="button-container">
-          {!answer && (
-            <>
-              <button style={{ width: noButtonSize }} onClick={() => handleAnswer('yes')}>
-                Yes
-              </button>
-              <button style={{ width: noButtonSize }} onClick={handleNoButtonClick}>
-                No
-              </button>
-            </>
-          )}
+          <button>Sí</button>
+          <button>No</button>
         </div>
       </main>
 
       <Footer />
     </div>
-  );
+  )
 }
