411d5c4cebb99143f3f0c178067d1ca881d4921d
LeonardoSebastianps <83441780+LeonardoSebastianps@users.noreply.github.com>
2024-02-13 21:42:21 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,11 @@
-// Importa el archivo CSS
-import '../styles/styles.css';
-
-// El resto de tu código _app.js
+import '../styles/globals.css'
 import Head from 'next/head'
-import Footer from '@components/Footer'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <>
       <Head>
         <title>TE AMO VAL</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
-      <Footer />
-    </div>
-  )
-}
-
-
-export default MyApp
+     
